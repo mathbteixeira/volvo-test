63d4634fe29926faabffe6e99d5b57fee770be66
Matheus Teixeira <mathbteixeira@gmail.com>
2021-11-06 23:34:51 -0300

    initial project structure

---
+++ src/main/java/com/volvo/volvotest/VolvoTestApplication.java
@@ -0,0 +1,13 @@
+package com.volvo.volvotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolvoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolvoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volvo/volvotest/VolvoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volvo.volvotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolvoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
